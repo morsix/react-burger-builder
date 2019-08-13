db80ad7964ca60392134f46e1a7719f19e6cbe43
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-13 11:07:58 +0100

    added layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-      Deli Burger
+      <Layout>
+        <p>Test</p>
+      </Layout>
       </div>
     )
   }
