1d2711e100a6c51b615cbfc11d00e6ba50ff724a
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-15 12:23:36 +0100

    unmounting interceptors from the hoc

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,14 +6,14 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
         constructor(props){
             super(props);
-            axios.interceptors.request.use(req => {
+            this.requestInterceptor = axios.interceptors.request.use(req => {
                 this.setState({
                     error: null
                 })
                 return req;
             })
 
-            axios.interceptors.response.use(res => res, error => {
+            this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({
                     error: error
                 })
@@ -22,6 +22,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
         
         state = {
             error: null
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         errorConfirmedHandler = () => {
