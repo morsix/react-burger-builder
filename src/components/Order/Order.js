0466d5ce7c86099c9d828770412ebad6a6c2f434
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-16 16:04:43 +0100

    added Orders component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Order.css"
+const order = (props) => {
+    return (
+        <div className="Order">
+            <p>Ingredients: Salad (1)</p>
+            <p>Price: <strong>GBP 6</strong></p>
+        </div>
+    )
+}
+
+export default order;
