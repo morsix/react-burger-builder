a6b2bc8acacb4aee5f7ded4e4a593706dbaa6bcc
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-27 15:27:50 +0100

    changed basename

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename='react-burger-builder'>
             <App></App>
         </BrowserRouter>
     </Provider>
