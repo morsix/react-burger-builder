46ff1b7086afa282d75e05f72859c456b4c57ef2
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-27 13:49:15 +0100

    unite tests for navigation items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(()=> {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('should should render 2 <navigationItem /> elements if not authenticated', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should should render 3 <navigationItem /> elements if authenticated', () => {
+        //wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
+
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+})
+
+
